86d15476d7f0ed6057d31cafe4de90498231dbf6
Alejandra Bedoya <alejandrabedoya00@gmail.com>
2021-03-25 13:55:40 -0500

    Controladores

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Reto Creativos
+
+Realizar las operaciones CRUD (Create, read, update, delete) para un formulario de usuarios
+que tiene la siguiente información:
+- Tipo de identificación
+- Identificación
+- Nombres
+- Apellidos
+- Correo
+- Rol [Administrador, usuario, cliente]
+- Celular
+- Contraseña (Se debe de guardar cifrada)
---
+++ server/controllers/usuarios.js
@@ -0,0 +1,132 @@
+const ServicePG = require("../services/postgres");
+
+let validar_usuario = (usuario) => {
+  if (!usuario) {
+    throw {
+      ok: false,
+      mensaje: "La información de la persona es obligatoria.",
+    };
+  }
+
+  if (!usuario.identificacion) {
+    throw { ok: false, mensaje: "La cédula es obligatoria." };
+  }
+  if (!usuario.nombres) {
+    throw { ok: false, mensaje: "El nombres es obligatorio." };
+  }
+  if (!usuario.apellidos) {
+    throw { ok: false, mensaje: "El apellidos es obligatorio." };
+  }
+};
+
+let guardar_usuario = async (usuario) => {
+  let _service = new ServicePG();
+  let sql = `INSERT INTO public.usuarios(
+              identificacion, tipo_identificacion, nombres, apellidos, correo, rol, celular, clave)
+              VALUES (
+                  $1,
+                  $2,
+                  $3,
+                  $4,
+                  $5,
+                  $6,
+                  $7,
+                  $8,
+                  md5($9)
+                  );`;
+
+  let values = [
+    usuario.identificacion,
+    usuario.tipo_identificacion,
+    usuario.nombres,
+    usuario.apellidos,
+    usuario.correo,
+    usuario.rol,
+    usuario.celular,
+    usuario.clave,
+  ];
+
+  let respuesta = await _service.runSql(sql, values);
+  return respuesta;
+};
+
+let ver_usuarios = async () => {
+  let _service = new ServicePG();
+  let sql = `SELECT usuarios.identificacion, 
+                    tipos_identificacion.nombre as "tipo_identificacion", 
+                    usuarios.nombres, usuarios.apellidos, usuarios.correo, 
+                    roles.nombre as "rol", usuarios.celular 
+                    FROM usuarios 
+                    INNER JOIN roles ON usuarios.rol = roles.id 
+                    INNER JOIN tipos_identificacion ON usuarios.tipo_identificacion = tipos_identificacion.id;`;
+  let respuesta = await _service.runSql(sql);
+  let result = respuesta.rows;
+  return result;
+};
+
+let ver_usuario = async (identificacion) => {
+  let _service = new ServicePG();
+  let sql = `SELECT usuarios.identificacion, 
+                    tipos_identificacion.nombre as "tipo_identificacion", 
+                    usuarios.nombres, usuarios.apellidos, usuarios.correo, 
+                    roles.nombre as "rol", usuarios.celular 
+                    FROM usuarios 
+                    INNER JOIN roles ON usuarios.rol = roles.id 
+                    INNER JOIN tipos_identificacion ON usuarios.tipo_identificacion = tipos_identificacion.id
+                    WHERE identificacion = $1;`;
+  let values = [identificacion];
+  let respuesta = _service.runSql(sql, values);
+  return respuesta;
+};
+
+let eliminar_usuario = (identificacion) => {
+  let _service = new ServicePG();
+  let sql = `DELETE FROM usuarios WHERE identificacion = $1`;
+  let values = [identificacion];
+  let respuesta = _service.runSql(sql, values);
+  return respuesta;
+};
+
+let editar_usuario = async (usuario, identificacion) => {
+  let _service = new ServicePG();
+  let sql = `UPDATE usuarios set tipo_identificacion = $1
+                 nombres = $2,
+                 apellidos = $3,
+                 correo = $4,
+                 rol = $5,
+                 celular = $6,
+                 WHERE identificacion= $7`;
+
+  let values = [
+    usuario.tipo_identificacion,
+    usuario.nombres,
+    usuario.apellidos,
+    usuario.correo,
+    usuario.rol,
+    usuario.celular,
+    identificacion,
+  ];
+  let respuesta = await _service.runSql(sql, values);
+  return respuesta;
+};
+
+let editar_clave = async (clave, identificacion) => {
+  let _service = new ServicePG();
+  let sql = `UPDATE usuarios set
+                 clave = md5($1)
+                 WHERE identificacion = $2`;
+
+  let values = [clave.clave_nueva, identificacion];
+  let respuesta = await _service.runSql(sql, values);
+  return respuesta;
+};
+
+module.exports = {
+  validar_usuario,
+  guardar_usuario,
+  eliminar_usuario,
+  editar_usuario,
+  ver_usuarios,
+  ver_usuario,
+  editar_clave,
+};
